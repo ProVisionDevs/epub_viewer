3edf21d9d347f09023f524829f0cc99045569066
ProVision Devs <provisiondevs@gmail.com>
2025-04-10 17:09:50 -0500

    Update epub_controller.dart
    Second attempt to resolve the error: type '_Map<Object?, Object?>' is not a subtype of type 'Map<String, dynamic>'

--- lib/src/epub_controller.dart
+++ lib/src/epub_controller.dart
@@ -58,18 +58,21 @@ class EpubController {
 
   ///Parsing chapters list form epub
   /// (PVD Fix) type '_Map<Object?, Object?>' is not a subtype of type 'Map<String, dynamic>'
-   Future<List<EpubChapter>> parseChapters() async {
-    if (_chapters.isNotEmpty) return _chapters;
-    checkEpubLoaded();
-    final result = await webViewController!.evaluateJavascript(source: 'getChapters()');
-  
-    _chapters = List<EpubChapter>.from(
-      (result as List).map(
-        (e) => EpubChapter.fromJson(Map<String, dynamic>.from(e as Map)),
-      ),
-    );
-    return _chapters;
-  }
+  Future<List<EpubChapter>> parseChapters() async {
+  if (_chapters.isNotEmpty) return _chapters;
+  checkEpubLoaded();
+
+  final result = await webViewController!.evaluateJavascript(source: 'getChapters()');
+
+  // ✅ This safely casts each chapter to Map<String, dynamic>
+  _chapters = List<EpubChapter>.from(
+    (result as List).map(
+      (e) => EpubChapter.fromJson(Map<String, dynamic>.from(e as Map)),
+    ),
+  );
+
+  return _chapters;
+}
 
   Completer searchResultCompleter = Completer<List<EpubSearchResult>>();
 
